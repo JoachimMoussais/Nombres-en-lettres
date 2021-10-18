13abf6049ab290795219101e9cb7fe2e48f072e5
Joachim Moussais <92729292+JoachimMoussais@users.noreply.github.com>
2021-10-18 14:49:43 +0200

    Exercice d'admission au Céfim : Nombres en lettres

---
+++ nombresenlettres.js
@@ -0,0 +1,20 @@
+
+        function compteur(nombre){
+			if (nombre == 0){
+				return "zero"
+			}
+            else if(nombre==1){
+                return "un"
+            }
+			else if(nombre==2){
+                return "deux"
+            }
+
+        }
+
+
+
+
+
+
+          
